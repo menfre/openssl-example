0c4a15452d4f36b95ab26ded9caea39caeb1c5f4
menfre <menfre@mac.com>
2016-09-12 10:57:42 +0200

    Implementet SHA 256, 512 to crypto.hpp. Added two tests for SHA-256 in crypto_test.cpp

--- tests/crypto_test.cpp
+++ tests/crypto_test.cpp
@@ -11,4 +11,13 @@ int main() {
   // Results copied from http://www.freecodeformat.com/pbkdf2.php
   assert(Crypto::hex(Crypto::pbkdf2("Password", "Salt", 4096, 128 / 8)) == "f66df50f8aaa11e4d9721e1312ff2e66");
   assert(Crypto::hex(Crypto::pbkdf2("Password", "Salt", 8192, 512 / 8)) == "a941ccbc34d1ee8ebbd1d34824a419c3dc4eac9cbc7c36ae6c7ca8725e2b618a6ad22241e787af937b0960cf85aa8ea3a258f243e05d3cc9b08af5dd93be046c");
+  
+  
+  // Tests by me.
+  
+  // SHA256
+  
+  assert(Crypto::hex(Crypto::sha256("Test")) == "532eaabd9574880dbf76b9b8cc00832c20a6ec113d682299550d7a6e0f345e25");
+  assert(Crypto::hex(Crypto::sha256("Test", 2)) == "c60907e990745f7d91c4423713764d2724571269d3db4856d37c6302792c59a6");
+  
 }
