216bbbb4763f1be7fc20d117f9fb5301b47228a1
menfre <menfre@mac.com>
2016-09-12 11:15:58 +0200

    Added some more tests

--- tests/crypto_test.cpp
+++ tests/crypto_test.cpp
@@ -20,4 +20,11 @@ int main() {
   assert(Crypto::hex(Crypto::sha256("Test")) == "532eaabd9574880dbf76b9b8cc00832c20a6ec113d682299550d7a6e0f345e25");
   assert(Crypto::hex(Crypto::sha256("Test", 2)) == "c60907e990745f7d91c4423713764d2724571269d3db4856d37c6302792c59a6");
   
+  // SHA-512
+  assert(Crypto::hex(Crypto::sha512("Test")) == "c6ee9e33cf5c6715a1d148fd73f7318884b41adcb916021e2bc0e800a5c5dd97f5142178f6ae88c8fdd98e1afb0ce4c8d2c54b5f37b30b7da1997bb33b0b8a31");
+  assert(Crypto::hex(Crypto::sha512("Test", 2)) == "1d114c408ea5a48bc013c6947b042b31706589852608b62b77ccfbdef879feddb1d18bc416fdf0e878a6f72b5ee131f1ef59bb6070201e46a07d1fbaaf1d03f7");
+  
+  // MD5
+  assert(Crypto::hex(Crypto::md5("Test")) == "0cbc6611f5540bd0809a388dc95a615b");
+  
 }
